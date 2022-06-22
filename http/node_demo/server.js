f1f3d8bfc8184021b7a62e35f8b76d9ef4db8d88
linyu134 <593553724@qq.com>
2022-06-22 09:49:22 +0800

    update

--- http/node_demo/server.js
+++ http/node_demo/server.js
@@ -24,17 +24,28 @@ var server = http.createServer(function(request, response){
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`二哈`)
+    response.write(`
+    <!DOTYPE html>
+    <html>
+    <head>
+    <link rel="stylesheet" href="./style">
+    <title>标题<title>
+    </head>
+    <body>
+    <h1>标题</h1>
+    </body>
+    </html>
+    `)
     response.end()
-  } else if(path === '/x'){
-    response.statusCode = 200
+  } else if(path === '/style'){
+    response.statusCode = 301
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
-    response.write(`body{color: red;}`)
+    response.write(`h1{color: red;}`)
     response.end()
   } else {
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`你输入的路径不存在对应的内容`)
+    response.write(`你访问的页面不存在`)
     response.end()
   }
 
